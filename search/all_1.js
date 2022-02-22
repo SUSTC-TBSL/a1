cd81b83562ac88da2783546cdf753a270422d977
foxieee24 <84270091+YZL24@users.noreply.github.com>
2022-02-22 23:50:04 +0800

    Add files via upload

---
+++ search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['copy_5fmatrix_0',['copy_matrix',['../assign1__mat_8c.html#ae04107c048797c58174db7b5d52de819',1,'copy_matrix(matrix mat):&#160;assign1_mat.c'],['../assign1__mat_8h.html#ae04107c048797c58174db7b5d52de819',1,'copy_matrix(matrix mat):&#160;assign1_mat.c']]],
+  ['create_5fmatrix_5fall_5fzero_1',['create_matrix_all_zero',['../assign1__mat_8c.html#a6f4b7c8910397a1ab891694485ebec56',1,'create_matrix_all_zero(int width, int height):&#160;assign1_mat.c'],['../assign1__mat_8h.html#a6f4b7c8910397a1ab891694485ebec56',1,'create_matrix_all_zero(int width, int height):&#160;assign1_mat.c']]]
+];
---
+++ search/all_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['fast_5fcal_5ffib_0',['fast_cal_fib',['../assign1_8h.html#a8b798924779a3f1c03e2fd56b7550b88',1,'assign1.h']]],
+  ['fast_5fmatrix_5fexp_1',['fast_matrix_exp',['../assign1_8h.html#a8a5a63462f9dea7aecc11d823261e0ad',1,'assign1.h']]]
+];
